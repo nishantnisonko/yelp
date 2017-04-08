c7c6cb5bbd2991ceed5357376ecb944144010d61
Nishant nishanko <nishantnishanko@Nishants-MacBook-Pro.local>
2017-04-08 14:23:30 -0700

    first commit

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -1,5 +1,5 @@
 //
-//  BusinessCell.swift
+//  Businessswift
 //  Yelp
 //
 //  Created by Nishant nishanko on 4/7/17.
@@ -10,9 +10,37 @@ import UIKit
 
 class BusinessCell: UITableViewCell {
 
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var reviewCountLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            addressLabel.text = business.address
+            categoriesLabel.text = business.categories
+            reviewCountLabel.text = "\(business.reviewCount!) Reviews"
+            distanceLabel.text = business.distance
+            thumbImageView.setImageWith(business.imageURL!)
+            ratingImageView.setImageWith(business.ratingImageURL!)
+            
+
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        thumbImageView.layer.cornerRadius = 3
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,22 +8,28 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController , UITableViewDataSource, UITableViewDelegate{
     
+    @IBOutlet weak var tableView: UITableView!
     var businesses: [Business]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
         
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
-            if let businesses = businesses {
-                for business in businesses {
-                    print(business.name!)
-                    print(business.address!)
-                }
-            }
+//            if let businesses = businesses {
+//                for business in businesses {
+//                    print(business.name!)
+//                    print(business.address!)
+//                }
+//            }
+            self.tableView.reloadData()
             
             }
         )
@@ -55,5 +61,22 @@ class BusinessesViewController: UIViewController {
      // Pass the selected object to the new view controller.
      }
      */
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses != nil{
+            return businesses!.count
+        }else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+        cell.business = businesses[indexPath.row]
+        return cell
+    }
+    
     
 }
